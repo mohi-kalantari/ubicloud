6d8cc4c81e6886642c861e7e1561a2ae519c1e6d
Eren Başak <eren@ubicloud.com>
2024-11-27 17:18:31 +0300

    fix params

--- routes/project/kubernetes.rb
+++ routes/project/kubernetes.rb
@@ -4,16 +4,16 @@ class Clover
   hash_branch(:project_prefix, "kubernetes-internal") do |r|
     r.post true do
       project = @project
-
-      puts project.id
       # authorize("Kubernetes:create", project.id)
       # fail Validation::ValidationFailed.new({billing_info: "Project doesn't have valid billing information"}) unless project.has_valid_payment_method?
+
+      params = JSON.parse(json_params)
 
       st = Prog::Vm::Nexus.assemble_with_sshable(
         "ubi",
         project.id,
         location: "hetzner-fsn1",
-        name: json_params["name"],
+        name: params["name"],
         size: "standard-2",
         storage_volumes: [
           {encrypted: true, size_gib: 30}
